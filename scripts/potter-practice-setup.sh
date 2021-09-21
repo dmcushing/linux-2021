8630f011973b50c22018844af5844e707495594c
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-09-21 14:53:58 -0400

    Update potter-practice-setup.sh

--- scripts/potter-practice-setup.sh
+++ scripts/potter-practice-setup.sh
@@ -4,6 +4,7 @@
 clear
 echo -e "Potter practice set up - ignore any errors below"
 echo -e "Enter the linuxuser password if prompted"
+mkdir /home/linuxuser/Hogwarts
 sudo chown -R linuxuser:linuxuser /home/linuxuser/Hogwarts
 sudo groupadd gryffindor
 sudo groupadd slytherin
@@ -19,12 +20,12 @@ sudo useradd -G slytherin,rowling -m -s /bin/bash draco
 sudo useradd -G muggles,rowling -m -s /bin/bash bryce
 sudo useradd -G muggles,rowling -m -s /bin/bash burbidge
 sudo useradd -G muggles,rowling -m -s /bin/bash bayliss
-potters=("harry" "hermione" "ron" "godric" "snapes" "lucius" "draco" "bryce" "burbidge" "bayliss")
+potters=( "harry" "hermione" "ron" "godric" "snapes" "lucius" "draco" "bryce" "burbidge" "bayliss" )
 
-for potters in ${potters[*]}
+for potters in ${potters[@]}
 do
 	echo $potters
-	echo -e "potter\npotter\n" | passwd $users >> /dev/null
+	echo -e "potter\npotter\n" | passwd $potters >> /dev/null
 done
 clear
 echo -e "All set..\n\n Time for quiddich"
