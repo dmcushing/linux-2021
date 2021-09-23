5bd58ddf27e10b016307d9b54bce3216be822223
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-09-23 09:26:21 -0400

    Update init-1025.sh

--- scripts/init-1025.sh
+++ scripts/init-1025.sh
@@ -1,8 +1,11 @@
 !#/bin/bash
+source /scripts/functions.sh
 clear
+is_super_userclear
+echo -e "Setting up..."
 useradd -m -s /bin/bash linuxuser
 chmod 660 /etc/sudoers
 echo "linuxuser ALL=(ALL:ALL) ALL" >> /etc/sudoers
 chmod 440 /etc/sudoers
 echo "linuxuser:cetystudent" | chpasswd
-echo "Init script... completed!"
+echo -e "Init script... completed!"
