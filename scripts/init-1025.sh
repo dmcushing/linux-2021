ebc90b763bdea10c7c38293b10055c28abd44f2a
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-09-25 16:05:58 -0400

    Update init-1025.sh

--- scripts/init-1025.sh
+++ scripts/init-1025.sh
@@ -1,11 +1,14 @@
 !#/bin/bash
 source /scripts/functions.sh
 clear
-is_super_userclear
+is_super_user
+clear
 echo -e "Setting up..."
 useradd -m -s /bin/bash linuxuser
 chmod 660 /etc/sudoers
 echo "linuxuser ALL=(ALL:ALL) ALL" >> /etc/sudoers
 chmod 440 /etc/sudoers
 echo "linuxuser:cetystudent" | chpasswd
+apt -y update
+apt -y install tree
 echo -e "Init script... completed!"
