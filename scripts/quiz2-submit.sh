cbfa26301db5ebdaf2144c1516e8ce25ba5e25a0
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-09-29 18:35:11 -0400

    quiz2 updates

---
+++ scripts/quiz2-submit.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Quiz 02 - Tom Hanks
+
+source /scripts/functions.sh
+
+# Gather Student Work
+
+clear
+is_super_user
+student_info Quiz 02
+
+tree /home/linuxuser/Quiz2 | tee -a $outfile
+check_owner 1 ~/Quiz2/TomHanks tomhanks
+check_owner 1 ~/Quiz2/TomHanks/2019 tomhanks
+check_owner 1 ~/Quiz2/TomHanks/2020 tomhanks
+check_owner 1 ~/Quiz2/TomHanks/2019/ABeautifulDay tomhanks
+check_owner 1 ~/Quiz2/TomHanks/2019/ToyStory4 tomhanks
+check_owner 1 ~/Quiz2/TomHanks/2020/BigCity tomhanks
+check_owner 1 ~/Quiz2/TomHanks/2020/Greyhound tomhanks
+check_owner 1 ~/Quiz2/TomHanks/2020/NewsoftheWorld tomhanks
+blank_line
+check_group 2 ~/Quiz2/TomHanks/2019 2019
+check_group 2 ~/Quiz2/TomHanks/2019/ABeautifulDay 2019
+blank_line
+check_group 3 ~/Quiz2/TomHanks/2020 2020
+check_group 3 ~/Quiz2/TomHanks/2020/BigCity 2020
+check_group 3 ~/Quiz2/TomHanks/2020/Greyhound 2020
+check_group 3 ~/Quiz2/TomHanks/2020/NewsoftheWorld 2020
+blank_line
+check_group 4 ~/Quiz2/TomHanks/2019/ToyStory4 toystory
+blank_line
+check_existence 5 ~/Quiz2/TomHanks/2019/ABeautifulDay f
+blank_line
+check_existence 6 ~/Quiz2/TomHanks/2020/SubsequentMovie f
+blank_line
+check_permissions 7 ~/Quiz2/TomHanks/2019/ABeautifulDay -rw-rw----
+check_permissions 7 ~/Quiz2/TomHanks/2019/ToyStory4 -rw-rw----
+blank_line
+check_permissions 8 ~/Quiz2/TomHanks/2019/ToyStory4 -rw-------
+#
+# Note: the name of the file should be 'geniuses'
+#
+mail_out Tom Hanks Quiz2
+
+
+
+
---
+++ scripts/quiz2.sh
@@ -0,0 +1,13 @@
+!#/bin/bash
+# Set Up for Tom Hanks Quiz
+source /scripts/functions.sh
+clear
+is_super_user
+echo -e "Setting up..."
+useradd -m -s /bin/bash tomhanks
+groupadd toystory
+groupadd 2020
+groupadd 2019
+mkdir /home/linuxuser/Quiz2
+chown linuxuser:linuxuser /home/linuxuser/Quiz2
+echo -e "Done setting up."
