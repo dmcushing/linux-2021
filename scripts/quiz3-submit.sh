8e270034f0085bae336a4edbfcbddcbbc7f52b78
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-10-07 06:18:03 -0400

    Update quiz3-submit.sh

--- scripts/quiz3-submit.sh
+++ scripts/quiz3-submit.sh
@@ -11,19 +11,20 @@ student_info Quiz 03 Postal Codes
 tree /home/linuxuser/Quiz3 | tee -a $outfile
 
 check_existence 1 /home/linuxuser/Quiz3/postalcodes.txt h
-
+blank_line
 check_existence 2 /home/linuxuser/Quiz3/dbury.txt f
 tail -5 /home/linuxuser/Quiz3/dbury.txt | tee -a $outfile
-
+blank_line
 check_existence 3 /home/linuxuser/Quiz3/A0K.txt f
 tail -5 /home/linuxuser/Quiz3/A0K.txt
-
+blank_line
 check_existence 4 /home/linuxuser/Quiz3/ag.txt f
 tail -5 /home/linuxuser/Quiz3/ag.txt
-
+blank_line
 check_existence 5 /home/linuxuser/Quiz3/test.txt f
 check_owner 5 /home/linuxuser/Quiz3/test.txt quiz3
 check_group 5 /home/linuxuser/Quiz3/test.txt quiz3
-check_perms 5 /home/linuxuser/Quiz3/test.txt -rw-r-----
+check_permissions 5 /home/linuxuser/Quiz3/test.txt -rw-r-----
+ls -l /home/linuxuser/Quiz3/test.txt | tee -a $outfile
 
 mail_out Postal Codes Quiz3
