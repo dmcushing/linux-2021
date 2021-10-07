9a6604da1fa531d564590e126d4de3a06883d506
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-10-06 20:05:55 -0400

    Create quiz3-setup.sh

---
+++ scripts/quiz3-setup.sh
@@ -0,0 +1,12 @@
+!#/bin/bash
+# Set Up for Postal Codes Quiz
+source /scripts/functions.sh
+clear
+is_super_user
+echo -e "Setting up..."
+useradd -m -s /bin/bash quiz3
+groupadd quiz3
+rm -Rf /home/linuxuser/Quiz3
+mkdir /home/linuxuser/Quiz3
+chown linuxuser:linuxuser /home/linuxuser/Quiz3
+echo -e "Done setting up."
