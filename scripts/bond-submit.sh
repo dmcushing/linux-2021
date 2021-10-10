aaae673377518f785a3f5d64a8c33e590e8d9ddb
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-10-10 08:22:17 -0400

    Update bond-submit.sh

--- scripts/bond-submit.sh
+++ scripts/bond-submit.sh
@@ -119,7 +119,7 @@ do
     FILES=("/home/bond/post80s/pierce/goldeneye-1995" "/home/bond/post80s/pierce/tomorrowneverdies-1997" "/home/bond/post80s/pierce/worldisnotenough-1999" "/home/bond/post80s/pierce/dieanotherday-2002")
     fi
     if [[ "$GROUPID" == "post80s" && "$USERID" == "daniel" ]]; then
-    FILES=("/home/bond/post80s/daniel/casinoroyale-2006" "/home/bond/post80s/daniel/quantumofsolace-2008" "/home/bond/post80s/daniel/skyfall-2012" "/home/bond/daniel/spectre-2015" "/home/bond/daniel/notimetodie-2021")
+    FILES=("/home/bond/post80s/daniel/casinoroyale-2006" "/home/bond/post80s/daniel/quantumofsolace-2008" "/home/bond/post80s/daniel/skyfall-2012" "/home/bond/daniel/post80s/spectre-2015" "/home/bond/daniel/post80s/notimetodie-2021")
     fi
     echo $GROUPID $USERID | tee -a $outfile
     echo " " | tee -a $outfile
@@ -202,7 +202,7 @@ if [ -e /home/linuxuser/bondassignment.tar.gz ]; then
     echo -e "file: /home/linuxuser/bondassignment.tar.gz - exists" | tee -a $outfile
     tar -tzvf /home/linuxuser/bondassignment.tar.gz | tail -5 | tee -a $outfile
 else
-    echo -e "file: /tmp/bondassignment.tar.gz - DOES NOT exist - try again" | tee -a $outfile
+    echo -e "file: /home/linuxuser/bondassignment.tar.gz - DOES NOT exist - try again" | tee -a $outfile
 fi
 
 # Submit the work
