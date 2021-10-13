b7badb1c52b3afab7895966798fa8fd0a8d1b78d
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-10-13 10:30:52 -0400

    Update quiz4-submit.sh

--- scripts/quiz4-submit.sh
+++ scripts/quiz4-submit.sh
@@ -11,10 +11,10 @@ student_info Quiz 03 Postal Codes
 tree /home/linuxuser/Quiz4 | tee -a $outfile
 
 check_existence 1 /home/linuxuser/Quiz4/polysort1.txt f
-tail -5 /home/linuxuser/Quiz4/polysort2.txt 2>/dev/null | tee -a $outfile
+head -5 /home/linuxuser/Quiz4/polysort1.txt 2>/dev/null | tee -a $outfile
 blank_line
 check_existence 2 /home/linuxuser/Quiz4/polysort2.txt f
-tail -5 /home/linuxuser/Quiz4/polysort2.txt 2>/dev/null| tee -a $outfile
+head -5 /home/linuxuser/Quiz4/polysort2.txt 2>/dev/null| tee -a $outfile
 blank_line
 
 mail_out Archiving and Stuff Quiz4
