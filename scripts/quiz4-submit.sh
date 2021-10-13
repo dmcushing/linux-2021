d3166677c20e3b4e6f2319259b777d51b6d880a5
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-10-13 10:49:33 -0400

    Update quiz4-submit.sh

--- scripts/quiz4-submit.sh
+++ scripts/quiz4-submit.sh
@@ -15,8 +15,23 @@ check_line_count 1 /home/linuxuser/Quiz4/polysort1.txt 20
 head -5 /home/linuxuser/Quiz4/polysort1.txt 2>/dev/null | tee -a $outfile
 blank_line
 check_existence 2 /home/linuxuser/Quiz4/polysort2.txt f
-check_line_count 2 /home/linuxuser/Quiz4/polysort1.txt 20
+check_line_count 2 /home/linuxuser/Quiz4/polysort2.txt 20
 head -5 /home/linuxuser/Quiz4/polysort2.txt 2>/dev/null| tee -a $outfile
 blank_line
+check_existence 3 /home/linuxuser/Quiz4/daemon.txt f
+head -5 /home/linuxuser/Quiz4/daemon.txt 2>/dev/null| tee -a $outfile
+blank_line
+check_existence 4 /home/linuxuser/Quiz4/etc.txt f
+head -5 /home/linuxuser/Quiz4/etc.txt 2>/dev/null| tee -a $outfile
+blank_line
+check_existence 4 /home/linuxuser/Quiz4/sda1.txt f
+head -5 /home/linuxuser/Quiz4/sda1.txt 2>/dev/null| tee -a $outfile
+blank_line
+check_existence 1 /home/linuxuser/Quiz4/reawords.txt f
+check_line_count 1 /home/linuxuser/Quiz4/reawords.txt 4
+head -5 /home/linuxuser/Quiz4/reawords.txt 2>/dev/null | tee -a $outfile
+blank_line
+check_existence 1 /home/linuxuser/quiz4.tar.gz f
+tar -tzvf /home/linuxuser/quiz4.tar.gz 2>/dev/null | tee -a $outfile
 
 mail_out Archiving and Stuff Quiz4
