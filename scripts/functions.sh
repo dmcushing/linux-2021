73422bd802a038b1181c5c3c9f33ed7be0b3b5b2
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-10-18 08:20:49 -0400

    Update functions.sh

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -218,7 +218,6 @@ fi
 ;;
 account_expiry)
 	userexpdate=`chage -l $3 | grep 'Account expires' | cut -d: -f2`
-	echo $userexpdate
 	if [ $userexpdate == "never" ]; then
         	echo -e "** ERROR ** Question $1: $3: INCORRECT account expiry date." | tee -a $outfile
 	else
