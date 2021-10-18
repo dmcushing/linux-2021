61ea21317acf8db4745a46ed31a1b1813958735a
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-10-18 18:53:17 -0400

    Update midterm03-submit.sh

--- scripts/midterm03-submit.sh
+++ scripts/midterm03-submit.sh
@@ -32,29 +32,29 @@ done
 blank_line
 
 echo -e "Users in Correct Groups:" | tee -a $outfile
-user_param 2 user_in_group gru human
-user_param 2 user_in_group gru basement
-user_param 2 user_in_group dave minion
-user_param 2 user_in_group dave basement
-user_param 2 user_in_group phil minion
-user_param 2 user_in_group phil blaster
+user_param 2 user_in_group captain human
+user_param 2 user_in_group captain earth
+user_param 2 user_in_group ironman human
+user_param 2 user_in_group ironman earth
+user_param 2 user_in_group thanos madtitan
+user_param 2 user_in_group thanos titan
 blank_line
 
 echo -e "Comments:" | tee -a $outfile
-user_param 2 comment gru "Evil Genius"
-user_param 2 comment dave "Best Minion"
-user_param 2 comment phil "Nemesis"
+user_param 2 comment captain "Chris Evans"
+user_param 2 comment ironman "Robert Downey Jr"
+user_param 2 comment thanos "Josh Brolin"
 blank_line
 echo -e "Shell:" | tee -a $outfile
 
-user_param 2 shell gru "/bin/bash"
-user_param 2 shell dave "/bin/bash"
-user_param 2 shell phil "/bin/bash"
+user_param 2 shell captain "/bin/bash"
+user_param 2 shell ironman "/bin/bash"
+user_param 2 shell thanos "/bin/bash"
 blank_line
 
 echo -e "Expiry Dates:" | tee -a $outfile
-user_param 2 account_expiry dave 2031-01-05
-user_param 2 account_expiry phil 2031-01-05
+user_param 2 account_expiry captain 2095-01-05
+user_param 2 account_expiry thanos 2095-01-05
 blank_line
 
 #
@@ -63,7 +63,7 @@ blank_line
 
 echo -e "Home Directories:" | tee -a $outfile
 
-DIRECTORY=("/home/gru" "/home/dave" "/home/phil")
+DIRECTORY=("/home/captain" "/home/ironman" "/home/thanos")
 for DIRECTORY in ${DIRECTORY[*]}
 do
 	check_existence 2 $DIRECTORY d
@@ -72,7 +72,7 @@ done
 blank_line
 echo -e "Directories:" | tee -a $outfile
 
-DIRECTORY=("/home/linuxuser/midterm" "/home/linuxuser/midterm/find" "/home/linuxuser/midterm/grep" "/home/linuxuser/midterm/grusplace" "/home/linuxuser/midterm/grusplace/basement")
+DIRECTORY=("/home/linuxuser/midterm" "/home/linuxuser/midterm/find" "/home/linuxuser/midterm/grep" "/home/linuxuser/midterm/earth" "/home/linuxuser/midterm/earth/desert")
 for DIRECTORY in ${DIRECTORY[*]}
 do
 	check_existence 3 $DIRECTORY d
@@ -85,38 +85,38 @@ blank_line
 
 echo -e "Changing Ownerships, Permissions, Moving:" | tee -a $outfile
 
-check_existence 4 ~/midterm/grusplace/basement/fartblaster f
-check_owner 4 ~/midterm/grusplace/basement/fartblaster dave
-check_group 4 ~/midterm/grusplace/basement/fartblaster minion
-check_permissions 4 ~/midterm/grusplace/basement/fartblaster -rw-r-----
-check_owner 4 ~/midterm/grusplace/basement dave
-check_group 4 ~/midterm/grusplace/basement minion
+check_existence 4 ~/midterm/earth/desert/timestone f
+check_owner 4 ~/midterm/earth/desert/timestone thanos
+check_group 4 ~/midterm/earth/desert/timestone madtitan
+check_permissions 4 ~/midterm/earth/desert/timestone -rw-r-----
+check_owner 4 ~/midterm/earth/desert thanos
+check_group 4 ~/midterm/earth/desert madtitan
 blank_line
 
 echo -e "Find:" | tee -a $outfile
 
-check_existence 5 ~/midterm/find/lmite.txt f
-check_line_count 5 ~/midterm/find/lmite.txt 3
-tail -5 ~/midterm/find/lmite.txt | tee -a $outfile
+check_existence 5 ~/midterm/find/nted.txt f
+check_line_count 5 ~/midterm/find/nted.txt 29
+tail -5 ~/midterm/find/nted.txt | tee -a $outfile
 blank_line
-check_existence 5 ~/midterm/find/baei.txt f
-check_line_count 5 ~/midterm/find/baei.txt 9
-tail -5 ~/midterm/find/baei.txt | tee -a $outfile
+check_existence 5 ~/midterm/find/reio.txt f
+check_line_count 5 ~/midterm/find/reio.txt 12
+tail -5 ~/midterm/find/reio.txt | tee -a $outfile
 blank_line
 
 echo -e "Grep:" | tee -a $outfile
 
-check_existence 6 ~/midterm/grep/antigonish.txt f
-check_line_count 6 ~/midterm/grep/antigonish.txt 121
-tail -5 ~/midterm/grep/antigonish.txt | tee -a $outfile
+check_existence 6 ~/midterm/grep/wolfville.txt f
+check_line_count 6 ~/midterm/grep/wolfville.txt 98
+tail -5 ~/midterm/grep/wolfville.txt | tee -a $outfile
 blank_line
-check_existence 6 ~/midterm/grep/B2G2.txt f
-check_line_count 6 ~/midterm/grep/B2G2.txt 59
-tail -5 ~/midterm/grep/B2G2.txt | tee -a $outfile
+check_existence 6 ~/midterm/grep/C1N6.txt f
+check_line_count 6 ~/midterm/grep/C1N6.txt 127
+tail -5 ~/midterm/grep/C1N6.txt | tee -a $outfile
 blank_line
-check_existence 6 ~/midterm/grep/horse.txt f
-check_line_count 6 ~/midterm/grep/horse.txt 1
-tail -5 ~/midterm/grep/horse.txt | tee -a $outfile
+check_existence 6 ~/midterm/grep/chicken.txt f
+check_line_count 6 ~/midterm/grep/chicken.txt 1
+tail -5 ~/midterm/grep/chicken.txt | tee -a $outfile
 blank_line
 
 echo -e "Miscellaneous:" | tee -a $outfile
@@ -130,20 +130,20 @@ blank_line
 check_existence 7 ~/midterm/freespace.txt f
 tail -3 ~/midterm/freespace.txt | tee -a $outfile
 blank_line
-if [ -d "~/midterm/grusplace/upstairs" ]; then
-	echo -e "Directory ~/midterm/grusplace/upstairs exists - you have to DELETE it." | tee -a $outfile
+if [ -d "~/midterm/earth/agamotto" ]; then
+	echo -e "Directory ~/midterm/earth/agamotto exists - you have to DELETE it." | tee -a $outfile
 else
-	echo -e "Directory ~/midterm/grusplace/upstairs doesn't exist - good." | tee -a $outfile
+	echo -e "Directory ~/midterm/earth/agamotto doesn't exist - good." | tee -a $outfile
 fi
 blank_line
 check_existence 7 ~/midterm/psaux.txt f
 tail -3 ~/midterm/psaux.txt | tee -a $outfile
 blank_line
-check_existence 7 ~/midterm/JP.txt f
-tail -5 ~/midterm/JP.txt | tee -a $outfile
+check_existence 7 ~/midterm/MM.txt f
+tail -5 ~/midterm/MM.txt | tee -a $outfile
 blank_line
 check_existence 7 /home/linuxuser/midterm/midterm.tar.gz f
 tar -tzvf /home/linuxuser/midterm/midterm.tar.gz | tee -a $outfile
 blank_line
 
-mail_out_test 02-Midterm
+mail_out_test 03-Midterm
