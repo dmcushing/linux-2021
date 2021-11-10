a49e83aa6e77c0a0f8caa96f3eef56ad26cc1481
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-11-10 08:01:10 -0500

    Update diskpart1-submit.sh

--- scripts/diskpart1-submit.sh
+++ scripts/diskpart1-submit.sh
@@ -12,5 +12,6 @@ lsblk -l -o NAME,SIZE,FSTYPE,MOUNTPOINT /dev/sdd /dev/sde | tee -a $outfile
 blank_line
 echo -e "/dev/sdd2 mounted read-only:" | tee -a $outfile
 mount | grep "sdd2" | tee -a $outfile
+blank_line
 # Submit the work
 mail_out Filesystes_and_Mountpoints
