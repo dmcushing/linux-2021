7a6244c7e47e136a79cb7bc08238861c05fa8456
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-11-10 05:34:17 -0500

    Update coffeed.sh

--- scripts/coffeed.sh
+++ scripts/coffeed.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #loop forever
 while :
-do           
-# sleep for 30 seconds          
-sleep 30          
-# wakeup send message to all users          
-wall Coffee time! 
+do   
+# sleep for 30 seconds
+sleep 30
+# wakeup send message to all users
+wall Coffee time!
 done
 
