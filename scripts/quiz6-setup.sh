2f620931596176c45fa3b47c3867726e62caf8e6
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-11-11 09:54:17 -0500

    Create quiz6-setup.sh

---
+++ scripts/quiz6-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Set Up for Quiz
+source /scripts/functions.sh
+clear
+is_super_user
+echo -e "Setting up..."
+rm -Rf /home/linuxuser/Quiz06
+mkdir /home/linuxuser/Quiz06
+chown linuxuser:linuxuser /home/linuxuser/Quiz06
+echo -e "Done setting up."
