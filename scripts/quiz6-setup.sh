5e588a384a5d72040ae73c816595efed95a03735
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-11-11 10:50:52 -0500

    quiz6

--- scripts/quiz6-setup.sh
+++ scripts/quiz6-setup.sh
@@ -4,6 +4,7 @@ source /scripts/functions.sh
 clear
 is_super_user
 echo -e "Setting up..."
+userdel -r quiz6 2>/dev/null
 rm -Rf /home/linuxuser/Quiz06
 mkdir /home/linuxuser/Quiz06
 chown linuxuser:linuxuser /home/linuxuser/Quiz06
---
+++ scripts/quiz6-submit.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Software Packages
+
+source /scripts/functions.sh
+
+# Gather Student Work
+
+clear
+is_super_user
+student_info Packages Quiz
+
+# Question 1
+package_check 1 figlet
+package_check 1 cowsay
+blank_line
+
+check_existence 1 ~/Quiz06/cowsay.txt f
+cat ~/Quiz06/cowsay.txt | tee -a $outfile
+blank_line
+
+check_existence 1 ~/Quiz06/figlet.txt f
+cat ~/Quiz06/figlet.txt | tee -a $outfile
+blank_line
+
+# Question 2
+check_existence 2 ~/Quiz06/figletfiles.txt f
+tail  -5 ~/Quiz06/figletfiles.txt | tee -a $outfile
+blank_line
+
+# Question 3
+check_existence 3 ~/Quiz06/homepage.txt f
+tail  -5 ~/Quiz06/homepage.txt | tee -a $outfile
+blank_line
+
+#Question 4
+entity_exists 4 quiz6 passwd
+user_param 4 account_expiry quiz6 2021-11-11
+check_existence 4 "/home/quiz6" d
+blank_line
+
+# Question 5
+check_existence 5 ~/quiz6.tar.gz f
+tar tzvf ~/quiz6.tar.gz 2</dev/null | tee -a $outfile
+blank_line
+
+mail_out Packages Quiz
