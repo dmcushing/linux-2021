325ee5066d02425b5e96e0c4c7b07d0a7ee7fa74
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-11-16 18:10:44 -0500

    lvm

---
+++ scripts/lvm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source /scripts/functions.sh
+clear
+is_super_user
+student_info LVM Assignment
+echo -e "Is the LVM created and mounted?" | tee -a $outfile
+lsblk -l -o NAME,SIZE,FSTYPE,MOUNTPOINT /dev/volgroupassign/lvm_assignment
+blank_line
+
+mail_out LVM Assignment
