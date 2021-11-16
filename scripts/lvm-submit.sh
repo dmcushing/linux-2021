b0dd271604cb9ebcf8b586aaa5cc5794817ee080
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-11-16 18:17:02 -0500

    Update lvm-submit.sh

--- scripts/lvm-submit.sh
+++ scripts/lvm-submit.sh
@@ -4,6 +4,7 @@ clear
 is_super_user
 student_info LVM Assignment
 echo -e "Is the LVM created and mounted?" | tee -a $outfile
+blank_line
 lsblk -l -o NAME,SIZE,FSTYPE,MOUNTPOINT /dev/volgroupassign/lv_assignment
 blank_line
 
