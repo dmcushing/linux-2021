f6b152d07972b8a9644e8acdfbadba7d11bd33d2
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-12-01 06:48:42 -0500

    Update cron-submit.sh

--- scripts/cron-submit.sh
+++ scripts/cron-submit.sh
@@ -5,6 +5,10 @@ is_super_user
 student_info cron Quiz
 echo -e "crontab entries:" | tee -a $outfile
 blank_line
+grep "^[^#;]" /var/spool/crontabs/root
+blank_line
+grep "^[^#;]" /var/spool/crontabs/linuxuser
+blank_line
 crontab -u root -l | tee -a $outfile
 blank_line
 
