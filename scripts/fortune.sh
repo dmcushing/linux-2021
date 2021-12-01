a7b7754d36b20eac9a15084985dad4056288f1c1
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-12-01 11:23:25 -0500

    quiz8

---
+++ scripts/fortune.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo -e " "
+echo -e " "
+echo -e "[[[[ XINETD FORTUNE SCRIPT ]]]]"
+echo -e " "
+echo -n "`/usr/games/fortune`"
+echo -e " "
+echo -e " "
---
+++ scripts/quiz8-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Set Up for Quiz
+source /scripts/functions.sh
+clear
+is_super_user
+echo -e "Setting up..."
+apt -y install xinetd.d telnet fortune 1&2>/dev/null
+rm -Rf /home/linuxuser/Quiz08
+mkdir /home/linuxuser/Quiz08
+chown linuxuser:linuxuser /home/linuxuser/Quiz08
+chmod 775 /home/linuxuser/Quiz08
+cp -f /scripts/fortune /etc/xinetd.d/
+cp -f /scripts/fortune.sh /etc/xinetd.d/
+echo -e "Done setting up."
