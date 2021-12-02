0e248cb53df33ecd03360e3a76a7c4742e5a2919
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-12-01 19:32:52 -0500

    quiz8

---
+++ scripts/quiz8-submit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+source /scripts/functions.sh
+clear
+is_super_user
+student_info LVM-Core Quiz
+echo -e "Is the LVM created and mounted?" | tee -a $outfile
+blank_line
+lsblk -l -o NAME,SIZE,FSTYPE,MOUNTPOINT /dev/vg_quiz8/lv_quiz8 | tee -a $outfile
+mount | grep 'quiz8' | tee -a $outfile
+blank_line
+vgdisplay vg_quiz8 | tee -a $outfile
+blank_line
+
+echo -e "The xinetd service is `systemctl is-enabled xinetd.service 2</dev/null`" | tee -a $outfile
+blank_line
+nc -z -vv localhost 1313
+nc -z -vv localhost 1313 >> $outfile 2>&1
+blank_line
+echo -e "Configuration File" | tee -a $outfile
+cat /etc/xinetd.d/fortune | tee -a $outfile
+blank_line
+
+mail_out LVM-Core Quiz
