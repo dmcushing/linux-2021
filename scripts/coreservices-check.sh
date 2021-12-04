f4bc031c23650c6beed0fad32ff5b0f3d99ab132
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-12-04 11:29:14 -0500

    Update coreservices-check.sh

--- scripts/coreservices-check.sh
+++ scripts/coreservices-check.sh
@@ -16,6 +16,8 @@ student_info Core Services Lab
    nc -z -vv localhost 5900 >> $outfile 2>&1
    blank_line
    echo -e "Configuration Files" | tee -a $outfile
+   grep -w '^telnet' /etc/services | tee -a $outfile
+   blank_line
    cat /etc/xinetd.d/testservice | tee -a $outfile
    blank_line
    cat /usr/bin/testservice.sh | tee -a $outfile
