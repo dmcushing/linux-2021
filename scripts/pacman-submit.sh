773ed0be576e02eb0578447c700e7b86472ca744
Dave Cushing <dave.cushing@cambriancollege.ca>
2021-12-20 13:02:50 -0500

    Update pacman-submit.sh

--- scripts/pacman-submit.sh
+++ scripts/pacman-submit.sh
@@ -4,9 +4,9 @@ clear
 is_super_user
 student_info PacMan Lab
 
-echo -e "All files should be in ~/quarantine - none in ~/Pacman"
+echo -e "All files should be in ~/quarantine - none in ~/Pacman" | tee -a $outfile
 blank_line
-find ~/ \( -name "sp00ky.txt" -o -name "CLYDE.txt" -o -name "8linky.txt" -o -name "PiNkY.txt" -o -name "1nky.txt" -o -name "sue.txt" -o -name "funky.txt" \)
+find ~/ \( -name "sp00ky.txt" -o -name "CLYDE.txt" -o -name "8linky.txt" -o -name "PiNkY.txt" -o -name "1nky.txt" -o -name "sue.txt" -o -name "funky.txt" \) | tee -a $outfile
 blank_line
 
 mail_out PacMan Lab
