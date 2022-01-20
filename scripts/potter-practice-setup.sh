2e120b3514273d5087e40093343c23906ede2b8b
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-01-20 15:07:36 -0500

    Update potter-practice-setup.sh

--- scripts/potter-practice-setup.sh
+++ scripts/potter-practice-setup.sh
@@ -7,12 +7,27 @@ is_super_user
 
 echo -e "Potter practice set up - ignore any errors below"
 echo -e "..Hogwart's Express leaving track 9 3/4"
+rm -Rf /home/linuxuser/Hogwarts
 mkdir /home/linuxuser/Hogwarts
 chown -R linuxuser:linuxuser /home/linuxuser/Hogwarts
+groupdel gryffindor &> /dev/null
+groupdel slytherin &> /dev/null
+groupdel muggles &> /dev/null
+groupdel rowling &> /dev/null
 groupadd gryffindor
 groupadd slytherin
 groupadd muggles
 groupadd rowling
+userdel -r harry &> /dev/null
+userdel -r hermione &> /dev/null
+userdel -r ron &> /dev/null
+userdel -r godric &> /dev/null
+userdel -r snapes &> /dev/null
+userdel -r lucius &> /dev/null
+userdel -r draco &> /dev/null
+userdel -r bryce &> /dev/null
+userdel -r burbidge &> /dev/null
+userdel -r bayliss &> /dev/null
 useradd -G gryffindor,rowling -m -s /bin/bash harry
 useradd -G gryffindor,rowling -m -s /bin/bash hermione
 useradd -G gryffindor,rowling -m -s /bin/bash ron
