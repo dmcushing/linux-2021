764d66a4899b86c873ca9a90e7cbf48a363013ed
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-03-03 06:24:19 -0500

    clean all

--- scripts/init-clean-all.sh
+++ scripts/init-clean-all.sh
@@ -9,6 +9,8 @@ git clone https://github.com/dmcushing/linux-2021.git /linux-2021/
 chmod 755 /linux-2021/scripts/*.sh
 chmod 755 /linux-2021/scripts/init-1025
 ln -s /linux-2021/scripts/ /scripts
+cd /home/linuxuser/
+rm -Rf *
 echo -e "...All done!"
 
 
