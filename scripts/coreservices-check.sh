3032767d112d4da29f425a751822ce66aa7510fb
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-03-30 14:34:47 -0400

    Update coreservices-check.sh

--- scripts/coreservices-check.sh
+++ scripts/coreservices-check.sh
@@ -6,19 +6,22 @@ is_super_user
 student_info Core Services Lab
 
 echo -e "1. Does mailperson exist?" | tee -a $outfile
+check_existence 2 /var/mail/mailperson f
 blank_line
 echo -e "2. Mail to mailperson in /var/mail/mailperson:" | tee -a $outfile
-check_existence 2 /var/mail/mailperson f
 tail -5 /var/mail/mailperson 2>/dev/null | tee -a $outfile
+blank_line
 echo -e "3. /var/www/html/index2.html created" | tee -a $outfile
 check_existence 3 /var/www/html/index2.html f
 tail -5 /var/www/html/index2.html 2>/dev/null | tee -a $outfile
+blank_line
 echo -e "4. grep /var/log/syslog for mailperson" | tee -a $outfile
 check_existence 4 ~/mailperson.log f
 tail -5 ~/mailperson.log 2>/dev/null | tee -a $outfile
+blank_line
 echo -e "5. grep /var/log/apache2/access.log for index2.html" | tee -a $outfile
 check_existence 4 ~/apache.log f
 tail -5 ~/apache.log 2>/dev/null | tee -a $outfile
-
+blank_line
 # Submit the work
 mail_out Core Services Lab
