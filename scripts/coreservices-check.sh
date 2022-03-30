5597d9995f0c7aefe320cf8c157cf51bda9481a3
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-03-30 14:32:17 -0400

    Update coreservices-check.sh

--- scripts/coreservices-check.sh
+++ scripts/coreservices-check.sh
@@ -5,24 +5,20 @@ clear
 is_super_user
 student_info Core Services Lab
 
-#
-# Do the services exist?
-#
-   echo -e "The xinetd service is `systemctl is-enabled xinetd.service 2</dev/null`" | tee -a $outfile
-   blank_line
-   nc -z -vv localhost 2323
-   nc -z -vv localhost 5900
-   nc -z -vv localhost 2323 >> $outfile 2>&1
-   nc -z -vv localhost 5900 >> $outfile 2>&1
-   blank_line
-   echo -e "Configuration Files" | tee -a $outfile
-   grep -w '^telnet' /etc/services | tee -a $outfile
-   blank_line
-   cat /etc/xinetd.d/testservice | tee -a $outfile
-   blank_line
-   cat /usr/bin/testservice.sh | tee -a $outfile
-   blank_line
-   cat /etc/xinetd.d/telnet | tee -a $outfile
-   blank_line
+echo -e "1. Does mailperson exist?" | tee -a $outfile
+blank_line
+echo -e "2. Mail to mailperson in /var/mail/mailperson:" | tee -a $outfile
+check_existence 2 /var/mail/mailperson f
+tail -5 /var/mail/mailperson 2>/dev/null | tee -a $outfile
+echo -e "3. /var/www/html/index2.html created" | tee -a $outfile
+check_existence 3 /var/www/html/index2.html f
+tail -5 /var/www/html/index2.html 2>/dev/null | tee -a $outfile
+echo -e "4. grep /var/log/syslog for mailperson" | tee -a $outfile
+check_existence 4 ~/mailperson.log f
+tail -5 ~/mailperson.log 2>/dev/null | tee -a $outfile
+echo -e "5. grep /var/log/apache2/access.log for index2.html" | tee -a $outfile
+check_existence 4 ~/apache.log f
+tail -5 ~/apache.log 2>/dev/null | tee -a $outfile
+
 # Submit the work
 mail_out Core Services Lab
