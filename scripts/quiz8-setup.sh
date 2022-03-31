a6948613911202ee68e06e96e73b77da6888252a
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-03-31 10:36:53 -0400

    Update quiz8-setup.sh

--- scripts/quiz8-setup.sh
+++ scripts/quiz8-setup.sh
@@ -7,10 +7,9 @@ echo -e "Setting up..."
 # apt -y install xinetd telnet telnetd fortune 2>&1>/dev/null
 rm -Rf /home/linuxuser/Quiz08
 mkdir /home/linuxuser/Quiz08
-mkdir -p /etc/xinetd.d 2>&1>/dev/null
 chown linuxuser:linuxuser /home/linuxuser/Quiz08
 chmod 775 /home/linuxuser/Quiz08
-cp -f /scripts/fortune /etc/xinetd.d/
-cp -f /scripts/fortune.sh /etc/xinetd.d/
-chmod 755 /etc/xinetd.d/fortune.sh
+sgdisk --zap-all /dev/sdb 
+sgdisk --zap-all /dev/sdc 
+sgdisk --zap-all /dev/sdd
 echo -e "Done setting up."
