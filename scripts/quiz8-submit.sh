b19222a2a0db0dec2a98b5d8c1c04a5bcf328b7e
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-03-31 18:43:12 -0400

    Update quiz8-submit.sh

--- scripts/quiz8-submit.sh
+++ scripts/quiz8-submit.sh
@@ -5,11 +5,11 @@ is_super_user
 student_info LVM Quiz
 echo -e "Is the LVM created and mounted?" | tee -a $outfile
 blank_line
-lsblk -l -o NAME,SIZE,FSTYPE,MOUNTPOINT /dev/vg_quiz8/lv_quiz8 | tee -a $outfile
+lsblk -l -o NAME,SIZE,FSTYPE,MOUNTPOINT /dev/vg_quiz/lv_quiz | tee -a $outfile
 blank_line
-mount | grep 'quiz8' | tee -a $outfile
+mount | grep 'quiz' | tee -a $outfile
 blank_line
-vgdisplay vg_quiz8 | tee -a $outfile
+vgdisplay vg_quiz | tee -a $outfile
 blank_line
 
 mail_out LVM Quiz
