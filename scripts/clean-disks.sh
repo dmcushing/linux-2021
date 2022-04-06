fb35a4ef833c3ece26bcef47e59c0352e87b326a
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-04-06 09:58:08 -0400

    Update clean-disks.sh

--- scripts/clean-disks.sh
+++ scripts/clean-disks.sh
@@ -16,19 +16,4 @@ sgdisk --zap-all /dev/sdf 2>/dev/null
 sgdisk --zap-all /dev/sdg 2>/dev/null
 sgdisk --zap-all /dev/sdh 2>/dev/null
 sgdisk --zap-all /dev/sdi 2>/dev/null
-sgdisk --print /dev/sdb | head -1 2>/dev/null
-sgdisk --print /dev/sdb | tail -2 2>/dev/null
-sgdisk --print /dev/sdc | head -1 2>/dev/null
-sgdisk --print /dev/sdc | tail -2 2>/dev/null
-sgdisk --print /dev/sdd | head -1 2>/dev/null
-sgdisk --print /dev/sdd | tail -2 2>/dev/null
-sgdisk --print /dev/sde | head -1 2>/dev/null
-sgdisk --print /dev/sde | tail -2 2>/dev/null
-sgdisk --print /dev/sdf | head -1 2>/dev/null
-sgdisk --print /dev/sdf | tail -2 2>/dev/null
-sgdisk --print /dev/sdg | head -1 2>/dev/null
-sgdisk --print /dev/sdg | tail -2 2>/dev/null
-sgdisk --print /dev/sdh | head -1 2>/dev/null
-sgdisk --print /dev/sdh | tail -2 2>/dev/null
-sgdisk --print /dev/sdi | head -1 2>/dev/null
-sgdisk --print /dev/sdi | tail -2 2>/dev/null
+rm -Rf /media/*
