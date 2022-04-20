1c97894bf07ae7719953314d6b00b67d80c86e71
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-04-20 06:53:49 -0400

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -8,9 +8,37 @@ is_super_user
 echo -e "Beginning Update..."
 git -C /linux-2021/ config user.email "dave@davecushing.ca"
 git -C /linux-2021/ config user.name "Dave Cushing"
-
+#
+# Update everything and make scripts executable
+#
+echo -e "Updating scripts.."
 git -C /linux-2021/ stash
 git -C /linux-2021/ pull origin
 chmod 777 /linux-2021/scripts/*.sh
 chmod 777 /linux-2021/scripts/init-1025
+#
+# Clean up disks
+#
+echo -e "Cleaning up disks.."
+echo -e "Unmounting and clearing LVM.."
+umount -a >/dev/null 2>&1
+vgremove -y $( vgdisplay --colon | cut -f1 -d: ) >/dev/null 2>&1
+pvremove -y $( pvdisplay -C -o pvname --noheadings | cut -f1 ) >/dev/null 2>&1
+echo -e "zap /dev/sdb"
+sgdisk --zap-all /dev/sdb >/dev/null 2>&1
+echo -e "zap /dev/sdc"
+sgdisk --zap-all /dev/sdc >/dev/null 2>&1
+echo -e "zap /dev/sdd"
+sgdisk --zap-all /dev/sdd >/dev/null 2>&1
+echo -e "zap /dev/sde"
+sgdisk --zap-all /dev/sde >/dev/null 2>&1
+echo -e "zap /dev/sdf"
+sgdisk --zap-all /dev/sdf >/dev/null 2>&1
+echo -e "zap /dev/sdg"
+sgdisk --zap-all /dev/sdg >/dev/null 2>&1
+echo -e "zap /dev/sdh"
+sgdisk --zap-all /dev/sdh >/dev/null 2>&1
+echo -e "zap /dev/sdi"
+sgdisk --zap-all /dev/sdi >/dev/null 2>&1
+rm -Rf /media/*
 echo -e "Done Update."
