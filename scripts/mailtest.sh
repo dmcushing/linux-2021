a0981bcff364cb27632d96588a9c7a1913256405
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-07-09 11:08:14 -0400

    Update mailtest.sh

--- scripts/mailtest.sh
+++ scripts/mailtest.sh
@@ -1,8 +1,14 @@
+echo -n "Enter your first name: "
+read fname
+echo -n "Enter your last name: "
+read lname
 echo -n "Enter your email address: "
 read mailaddy
+fname=`echo $fname | sed 's/ /_/g'`
+lname=`echo $lname | sed 's/ /_/g'`
 
 curl --request POST \
   --url https://api.sendgrid.com/v3/mail/send \
   --header "Authorization: Bearer $SENDGRID_API_KEY" \
   --header 'Content-Type: application/json' \
-  --data '{"personalizations": [{"to": [{"email": "'"$mailaddy"'"}]}],"from": {"email": "dave@davecushing.ca"},"subject": "CET1025 Test Email","content": [{"type": "text/plain", "value": "This is a test."}]}'
+  --data '{"personalizations": [{"to": [{"email": "'"$mailaddy"'"}],"cc": [{"email":"dave@davecushing.ca"}]}],"from": {"email": "dave@davecushing.ca"},"subject": "'"Email from $fname $lname"'","content": [{"type": "text/plain", "value": "This is a test."}]}'
