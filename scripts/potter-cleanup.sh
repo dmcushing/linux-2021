ff99fba7368ccb90a2342fc809725fccf6be7786
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-07-19 05:42:25 -0400

    potter fixes

--- scripts/potter-cleanup.sh
+++ scripts/potter-cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Potter Practice - Cleanup Script
+# Potter Cleanup Script
 
 source /scripts/functions.sh
 clear
@@ -7,12 +7,11 @@ is_super_user
 
 echo -e "Potter practice clean up"
 rm -Rf /home/linuxuser/Hogwarts
-rm -Rf /home/linuxuser/assignment1
 userdel -r harry &> /dev/null
 userdel -r hermione &> /dev/null
 userdel -r ron &> /dev/null
 userdel -r godric &> /dev/null
-userdel -r snapes &> /dev/null
+userdel -r snape &> /dev/null
 userdel -r lucius &> /dev/null
 userdel -r draco &> /dev/null
 userdel -r bryce &> /dev/null
--- scripts/potter-setup.sh
+++ scripts/potter-setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Potter Practice - Setup Script
+# Potter Setup Script
 
 source /scripts/functions.sh
 clear
 is_super_user
 
-echo -e "Potter practice set up - ignore any errors below"
+echo -e "Potter set up - ignore any errors below"
 echo -e "..Hogwart's Express leaving track 9 3/4"
 rm -Rf /home/linuxuser/Hogwarts
 mkdir /home/linuxuser/Hogwarts
@@ -15,15 +15,15 @@ groupdel gryffindor &> /dev/null
 groupdel slytherin &> /dev/null
 groupdel muggles &> /dev/null
 groupdel rowling &> /dev/null
-groupadd gryffindor
-groupadd slytherin
-groupadd muggles
-groupadd rowling
+groupadd gryffindor  &> /dev/null
+groupadd slytherin  &> /dev/null
+groupadd muggles  &> /dev/null
+groupadd rowling  &> /dev/null
 userdel -r harry &> /dev/null
 userdel -r hermione &> /dev/null
 userdel -r ron &> /dev/null
 userdel -r godric &> /dev/null
-userdel -r snapes &> /dev/null
+userdel -r snape &> /dev/null
 userdel -r lucius &> /dev/null
 userdel -r draco &> /dev/null
 userdel -r bryce &> /dev/null
@@ -33,14 +33,14 @@ useradd -G gryffindor,rowling -m -s /bin/bash harry
 useradd -G gryffindor,rowling -m -s /bin/bash hermione
 useradd -G gryffindor,rowling -m -s /bin/bash ron
 useradd -G gryffindor,rowling -m -s /bin/bash godric
-useradd -G slytherin,rowling -m -s /bin/bash snapes
+useradd -G slytherin,rowling -m -s /bin/bash snape
 useradd -G slytherin,rowling -m -s /bin/bash lucius
 useradd -G slytherin,rowling -m -s /bin/bash draco
 useradd -G muggles,rowling -m -s /bin/bash bryce
 useradd -G muggles,rowling -m -s /bin/bash burbage
 useradd -G muggles,rowling -m -s /bin/bash bayliss
 
-potters=("harry" "hermione" "ron" "godric" "snapes" "lucius" "draco" "bryce" "burbage" "bayliss")
+potters=("harry" "hermione" "ron" "godric" "snape" "lucius" "draco" "bryce" "burbage" "bayliss")
 
 for potters in ${potters[*]}
 do
