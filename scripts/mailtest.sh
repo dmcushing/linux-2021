d3ada3242568c0790ece3bc6b0caabe56f4f3f05
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-08-30 19:26:39 -0400

    Update mailtest.sh

--- scripts/mailtest.sh
+++ scripts/mailtest.sh
@@ -6,6 +6,8 @@ do
 	echo $alldone
 	echo -n "Enter INSTRUCTOR email address: "
 	read inmailaddy
+	echo -n "     Enter your student number: "
+	read snumber
 	echo -n "         Enter your first name: "
 	read fname
 	echo -n "          Enter your last name: "
@@ -25,8 +27,17 @@ done
 fname=`echo $fname | sed 's/ /_/g'`
 lname=`echo $lname | sed 's/ /_/g'`
 
+cat << EOF > ~/.info/.info
+Name:$fname $lname
+FName:$fname
+LName:$lname
+Email:$mailaddy
+Student:$snumber
+Instructor:$inmailaddy
+EOF
+
 curl --request POST \
   --url https://api.sendgrid.com/v3/mail/send \
   --header "Authorization: Bearer $SENDGRID_API_KEY" \
   --header 'Content-Type: application/json' \
-  --data '{"personalizations": [{"to": [{"email": "'"$mailaddy"'"}],"cc": [{"email":"dave@davecushing.ca"}]}],"from": {"email": "dave@davecushing.ca"},"subject": "'"Email from $fname $lname"'","content": [{"type": "text/plain", "value": "This is a test."}]}'
+  --data '{"personalizations": [{"to": [{"email": "'"$mailaddy"'"}],"cc": [{"email":"'"$inmailaddy"'"}]}],"from": {"email": "'"$inmailaddy"'"},"subject": "'"Email from $fname $lname"'","content": [{"type": "text/plain", "value": "This is a test."}]}'
