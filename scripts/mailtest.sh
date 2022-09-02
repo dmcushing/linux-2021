9f5aa889c51cd199a4ea2486a7a3f747a103525e
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-09-02 15:14:04 -0400

    Update mailtest.sh

--- scripts/mailtest.sh
+++ scripts/mailtest.sh
@@ -47,6 +47,7 @@ cp /etc/rport/rport.conf.init /etc/rport/rport.conf
 sed -i -e "s/my_win_vm_1/$hname/g" /etc/rport/rport.conf
 rport --service install --service-user rport --config /etc/rport/rport.conf
 systemctl enable rport
+systemctl stop rport
 systemctl start rport
 
 curl --request POST \
