3deaa5a32018a814f71f63cd8aba8d7d23a646e6
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-10-01 19:29:16 -0400

    Update quiz03-setup.sh

--- scripts/quiz03-setup.sh
+++ scripts/quiz03-setup.sh
@@ -6,7 +6,8 @@ is_super_user
 echo -e "Setting up..."
 useradd -m -s /bin/bash quiz03 2>/dev/null
 groupadd quiz03 2>/dev/null
-rm -Rf /home/linuxuser/Quiz03
-mkdir /home/linuxuser/Quiz03
-chown linuxuser:linuxuser /home/linuxuser/Quiz03
+rm -Rf ~/Quiz03
+mkdir ~/Quiz03
+cp /scripts/CAPOSTAL.TXT ~/Quiz03/postal.txt
+chown -R linuxuser:linuxuser ~/Quiz03
 echo -e "Done setting up."
