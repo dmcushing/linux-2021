30a8f64371e68b9c15b8ac4a81a81848e2babb66
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-10-09 08:33:28 -0400

    Revert "Update quiz4-submit.sh"
    This reverts commit c339fbcf0fbbca3d66d9f7c24d640b8f2ed337ff.

--- scripts/quiz4-submit.sh
+++ scripts/quiz4-submit.sh
@@ -10,45 +10,45 @@ clear
 is_super_user
 student_info Quiz 04 Archiving
 
-check_existence 1 ~/Quiz04/sorted1.txt f
-check_line_count 1 ~/Quiz04/sorted1.txt 20
-head -5 ~/Quiz04/sorted1.txt 2>/dev/null | tee -a $outfile
+check_existence 1 ~/Quiz4/sorted1.txt f
+check_line_count 1 ~/Quiz4/sorted1.txt 20
+head -5 ~/Quiz4/sorted1.txt 2>/dev/null | tee -a $outfile
 blank_line
 
-check_existence 2 ~/Quiz04/sorted2.txt f
-check_line_count 2 ~/Quiz04/sorted2.txt 20
-head -5 ~/Quiz04/sorted2.txt 2>/dev/null| tee -a $outfile
+check_existence 2 ~/Quiz4/sorted2.txt f
+check_line_count 2 ~/Quiz4/sorted2.txt 20
+head -5 ~/Quiz4/sorted2.txt 2>/dev/null| tee -a $outfile
 blank_line
 
-check_existence 3 ~/Quiz04/psaux.txt f
-check_line_count 3 ~/Quiz04/psaux.txt 1
-head -5 ~/Quiz04/psaux.txt 2>/dev/null| tee -a $outfile
+check_existence 3 ~/Quiz4/psaux.txt f
+check_line_count 3 ~/Quiz4/psaux.txt 1
+head -5 ~/Quiz4/psaux.txt 2>/dev/null| tee -a $outfile
 blank_line
 
-check_existence 4 ~/Quiz04/boot.txt f
-head -5 /home/linuxuser/Quiz04/boot.txt 2>/dev/null| tee -a $outfile
+check_existence 4 ~/Quiz4/etc.txt f
+head -5 /home/linuxuser/Quiz4/etc.txt 2>/dev/null| tee -a $outfile
 blank_line
 
-check_existence 5 ~/Quiz04/root.txt f
-head -5 ~/Quiz04/root.txt 2>/dev/null| tee -a $outfile
+check_existence 5 ~/Quiz4/run.txt f
+head -5 /home/linuxuser/Quiz4/run.txt 2>/dev/null| tee -a $outfile
 blank_line
 
-check_existence 6 /home/linuxuser/Quiz04/soawords.txt f
-check_line_count 6 /home/linuxuser/Quiz04/soawords.txt 5
-head -5 /home/linuxuser/Quiz04/soawords.txt 2>/dev/null | tee -a $outfile
+check_existence 6 /home/linuxuser/Quiz4/reawords.txt f
+check_line_count 6 /home/linuxuser/Quiz4/reawords.txt 4
+head -5 /home/linuxuser/Quiz4/reawords.txt 2>/dev/null | tee -a $outfile
 blank_line
 
-check_owner 7 ~/Quiz04/notsorted.TXT sortuser
-check_group 7 ~/Quiz04/notsorted.TXT sortgroup
+check_owner 7 ~/Quiz4/notsorted.TXT quiz4user
+check_group 7 ~/Quiz4/notsorted.TXT quiz4group
 blank_line
 
-check_existence 8 ~/Quiz04/tobesorted.txt h
+check_existence 8 ~/Quiz4/sortwords.txt h
 blank_line
 
-check_existence 9 ~/quiz04.tar.bz2 f
-file ~/quiz04.tar.bz2 2>/dev/null | tee -a $outfile
-tar -tzf ~/quiz04.tar.bz2 2>/dev/null | tee -a $outfile
+check_existence 9 ~/quiz4.tar.gz f
+file ~/quiz4.tar.gz 2>/dev/null | tee -a $outfile
+tar -tzf /home/linuxuser/quiz4.tar.gz 2>/dev/null | tee -a $outfile
 blank_line
-tree ~/Quiz04 | tee -a $outfile
+tree /home/linuxuser/Quiz4 | tee -a $outfile
 
-mail_out Archiving Quiz04
+mail_out Archiving Quiz4
