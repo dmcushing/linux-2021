92e85de3e115852afc133094e8c62345fe1058fe
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-10-09 07:35:45 -0400

    Update quiz4-submit.sh

--- scripts/quiz4-submit.sh
+++ scripts/quiz4-submit.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Quiz 02 - Tom Hanks
+# Quiz 04 - Archiving
+
 
 source /scripts/functions.sh
 
@@ -7,31 +8,47 @@ source /scripts/functions.sh
 
 clear
 is_super_user
-student_info Quiz 03 Postal Codes
-tree /home/linuxuser/Quiz4 | tee -a $outfile
+student_info Quiz 04 Archiving
 
-check_existence 1 /home/linuxuser/Quiz4/polysort1.txt f
-check_line_count 1 /home/linuxuser/Quiz4/polysort1.txt 20
-head -5 /home/linuxuser/Quiz4/polysort1.txt 2>/dev/null | tee -a $outfile
+check_existence 1 ~/Quiz4/sorted1.txt f
+check_line_count 1 ~/Quiz4/sorted1.txt 20
+head -5 ~/Quiz4/sorted1.txt 2>/dev/null | tee -a $outfile
 blank_line
-check_existence 2 /home/linuxuser/Quiz4/polysort2.txt f
-check_line_count 2 /home/linuxuser/Quiz4/polysort2.txt 20
-head -5 /home/linuxuser/Quiz4/polysort2.txt 2>/dev/null| tee -a $outfile
+
+check_existence 2 ~/Quiz4/sorted2.txt f
+check_line_count 2 ~/Quiz4/sorted2.txt 20
+head -5 /Quiz4/sorted2.txt 2>/dev/null| tee -a $outfile
 blank_line
-check_existence 3 /home/linuxuser/Quiz4/daemon.txt f
-head -5 /home/linuxuser/Quiz4/daemon.txt 2>/dev/null| tee -a $outfile
+
+check_existence 3 ~/Quiz4/psaux.txt f
+check_line_count 3 ~/Quiz4/psaux.txt 1
+head -5 ~/Quiz4/psaux.txt 2>/dev/null| tee -a $outfile
 blank_line
-check_existence 4 /home/linuxuser/Quiz4/etc.txt f
+
+check_existence 4 ~/Quiz4/etc.txt f
 head -5 /home/linuxuser/Quiz4/etc.txt 2>/dev/null| tee -a $outfile
 blank_line
-check_existence 5 /home/linuxuser/Quiz4/sda1.txt f
-head -5 /home/linuxuser/Quiz4/sda1.txt 2>/dev/null| tee -a $outfile
+
+check_existence 5 ~/Quiz4/run.txt f
+head -5 /home/linuxuser/Quiz4/run.txt 2>/dev/null| tee -a $outfile
 blank_line
+
 check_existence 6 /home/linuxuser/Quiz4/reawords.txt f
 check_line_count 6 /home/linuxuser/Quiz4/reawords.txt 4
 head -5 /home/linuxuser/Quiz4/reawords.txt 2>/dev/null | tee -a $outfile
 blank_line
-check_existence 7 /home/linuxuser/quiz4.tar.gz f
-tar -tzvf /home/linuxuser/quiz4.tar.gz 2>/dev/null | tee -a $outfile
 
-mail_out Archiving_and_Stuff Quiz4
+check_owner 7 ~/Quiz4/notsorted.TXT quiz4user
+check_group 7 ~/Quiz4/notsorted.TXT quiz4group
+blank_line
+
+check_existence 8 ~/Quiz4/sortwords.txt h
+blank_line
+
+check_existence 9 ~/quiz4.tar.gz f
+file ~/quiz4.tar.gz 2>/dev/null | tee -a $outfile
+tar -tzvf /home/linuxuser/quiz4.tar.gz 2>/dev/null | tee -a $outfile
+blank_line
+tree /home/linuxuser/Quiz4 | tee -a $outfile
+
+mail_out Archiving Quiz4
