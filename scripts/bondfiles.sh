a99f4dd2550aa24ebabf392f53e4ac835776ccd7
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-10-09 12:28:44 -0400

    Update bondfiles.sh

--- scripts/bondfiles.sh
+++ scripts/bondfiles.sh
@@ -34,13 +34,13 @@ sudo useradd -m -G post80s,bonds -c "George Lazenby" -s /bin/bash george
 sudo useradd -m -G pre80s,post80s,bonds -c "Roger Moore" -s /bin/bash roger
 
 # Create the file structure
-seanFILES=("~/bond/pre80s/sean/drno-1962" "~/bond/pre80s/sean/fromrussiawithlove-1963" "~/bond/pre80s/sean/goldfinger-1964" "~/bond/pre80s/sean/thunderball-1965" "~/bond/pre80s/sean/youonlylivetwice-1967" "~/bond/pre80s/sean/diamondsareforever-1971")
-georgeFILES=("~/bond/pre80s/george/onhermajestyssecretservice-1969")
-pre80srogerFILES=("~/bond/roger/liveandletdie-1973" "~/bond/roger/manwiththegoldengun-1974" "~/bond/roger/spywholovedme-1977" "~/bond/roger/moonraker-1979")
-post80srogerFILES=("~/bond/roger/foryoureyesonly-1981" "~/bond/roger/octopussy-1983" "~/bond/roger/aviewtoakill-1985") 
-timothyFILES=("~/bond/post80s/timothy/livingdaylights-1987" "~/bond/post80s/timothy/licencetokill-1989")
-pierceFILES=("~/bond/post80s/pierce/goldeneye-1995" "~/bond/post80s/pierce/tomorrowneverdies-1997" "~/bond/post80s/pierce/worldisnotenough-1999" "~/bond/post80s/pierce/dieanotherday-2002" "~/bond/post80s/pierce/script-run")
-danielFILES=("~/bond/post80s/daniel/casinoroyale-2006" "~/bond/post80s/daniel/quantumofsolace-2008" "~/bond/post80s/daniel/skyfall-2012" "~/bond/post80s/daniel/spectre-2015" "~/bond/post80s/daniel/notimetodie-2021")
+seanFILES=(~/bond/pre80s/sean/drno-1962 ~/bond/pre80s/sean/fromrussiawithlove-1963 ~/bond/pre80s/sean/goldfinger-1964 ~/bond/pre80s/sean/thunderball-1965 ~/bond/pre80s/sean/youonlylivetwice-1967 ~/bond/pre80s/sean/diamondsareforever-1971)
+georgeFILES=(~/bond/pre80s/george/onhermajestyssecretservice-1969)
+pre80srogerFILES=(~/bond/roger/liveandletdie-1973 ~/bond/roger/manwiththegoldengun-1974 ~/bond/roger/spywholovedme-1977 ~/bond/roger/moonraker-1979)
+post80srogerFILES=(~/bond/roger/foryoureyesonly-1981 ~/bond/roger/octopussy-1983 ~/bond/roger/aviewtoakill-1985) 
+timothyFILES=(~/bond/post80s/timothy/livingdaylights-1987 ~/bond/post80s/timothy/licencetokill-1989)
+pierceFILES=(~/bond/post80s/pierce/goldeneye-1995 ~/bond/post80s/pierce/tomorrowneverdies-1997 ~/bond/post80s/pierce/worldisnotenough-1999 ~/bond/post80s/pierce/dieanotherday-2002 ~/bond/post80s/pierce/script-run)
+danielFILES=(~/bond/post80s/daniel/casinoroyale-2006 ~/bond/post80s/daniel/quantumofsolace-2008 ~/bond/post80s/daniel/skyfall-2012 ~/bond/post80s/daniel/spectre-2015 ~/bond/post80s/daniel/notimetodie-2021)
 
 for seanFILES in ${seanFILES[*]}
 do
