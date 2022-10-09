65ed411550df150ff6ca29a2e719e58b00a8fdb7
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-10-09 06:41:38 -0400

    Update quiz4-setup.sh

--- scripts/quiz4-setup.sh
+++ scripts/quiz4-setup.sh
@@ -4,9 +4,10 @@ source /scripts/functions.sh
 clear
 is_super_user
 echo -e "Setting up..."
-useradd -m -s /bin/bash quiz4 2>/dev/null
-groupadd quiz4 2>/dev/null
-rm -Rf /home/linuxuser/Quiz4
-mkdir /home/linuxuser/Quiz4
-chown linuxuser:linuxuser /home/linuxuser/Quiz4
+useradd -m -s /bin/bash quiz4user 2>/dev/null
+groupadd quiz4group 2>/dev/null
+rm -Rf ~/Quiz4
+mkdir ~/Quiz4
+cp /scripts/work/polyphons ~/Quiz4/notsorted.TXT
+chown -R linuxuser:linuxuser ~/Quiz4
 echo -e "Done setting up."
