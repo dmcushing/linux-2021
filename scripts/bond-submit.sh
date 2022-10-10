97e509f0ac99158bb1edf5722a45828c9fdaf321
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-10-10 07:54:20 -0400

    bond files

--- scripts/bond-submit.sh
+++ scripts/bond-submit.sh
@@ -5,7 +5,9 @@ clear
 source /scripts/functions.sh
 is_super_user
 student_info Bond Lab
-DIRECTORY=("/home/bond/roger" "/home/bond/pre80s/sean" "/home/bond/pre80s/george" "/home/bond/post80s/timothy" "/home/bond/post80s/pierce" "/home/bond/post80s/daniel")
+
+BASEDIR="/home/linuxuser/bond"
+DIRECTORY=("$BASEDIR/roger" "$BASEDIR/pre80s/sean" "$BASEDIR/pre80s/george" "$BASEDIR/post80s/timothy" "$BASEDIR/post80s/pierce" "$BASEDIR/post80s/daniel")
 for DIRECTORY in ${DIRECTORY[*]}
 do
     if [ -d "$DIRECTORY" ]; then
@@ -13,7 +15,7 @@ do
     else
                 echo -e "Directory $DIRECTORY doesn't exist - please check" | tee -a $outfile
 fi
-if [[ "`stat -c %A $DIRECTORY`" == "drwxr-xr-x" ]]; then
+if [[ "`stat -c %A $DIRECTORY`" == "drwxrwxr-x" ]]; then
  	echo -e "$DIRECTORY - correct permissions" | tee -a $outfile
 else
         echo -e "$DIRECTORY - incorrect permissions, try again." | tee -a $outfile
@@ -96,25 +98,25 @@ do
         continue
     fi
     if [[ "$GROUPID" == "pre80s" && "$USERID" == "sean" ]]; then
-        FILES=("/home/bond/pre80s/sean/drno-1962" "/home/bond/pre80s/sean/fromrussiawithlove-1963" "/home/bond/pre80s/sean/goldfinger-1964" "/home/bond/pre80s/sean/thunderball-1965" "/home/bond/pre80s/sean/youonlylivetwice-1967" "/home/bond/pre80s/sean/diamondsareforever-1971")
+        FILES=("$BASEDIR/pre80s/sean/drno-1962" "$BASEDIR/pre80s/sean/fromrussiawithlove-1963" "$BASEDIR/pre80s/sean/goldfinger-1964" "$BASEDIR/pre80s/sean/thunderball-1965" "$BASEDIR/pre80s/sean/youonlylivetwice-1967" "$BASEDIR/pre80s/sean/diamondsareforever-1971")
     fi
     if [[ "$GROUPID" == "pre80s" && "$USERID" == "george" ]]; then
-    FILES=("/home/bond/pre80s/george/onhermajestyssecretservice-1969")
+    FILES=("$BASEDIR/pre80s/george/onhermajestyssecretservice-1969")
     fi
     if [[ "$GROUPID" == "pre80s" && "$USERID" == "roger" ]]; then
-    FILES=("/home/bond/roger/liveandletdie-1973" "/home/bond/roger/manwiththegoldengun-1974" "/home/bond/roger/spywholovedme-1977" "/home/bond/roger/moonraker-1979")
+    FILES=("$BASEDIR/roger/liveandletdie-1973" "$BASEDIR/roger/manwiththegoldengun-1974" "$BASEDIR/roger/spywholovedme-1977" "$BASEDIR/roger/moonraker-1979")
     fi
     if [[ "$GROUPID" == "post80s" && "$USERID" == "roger" ]]; then
-    FILES=("/home/bond/roger/foryoureyesonly-1981" "/home/bond/roger/octopussy-1983" "/home/bond/roger/aviewtoakill-1985") 
+    FILES=("$BASEDIR/roger/foryoureyesonly-1981" "$BASEDIR/roger/octopussy-1983" "$BASEDIR/roger/aviewtoakill-1985") 
     fi
     if [[ "$GROUPID" == "post80s" && "$USERID" == "timothy" ]]; then
-    FILES=("/home/bond/post80s/timothy/livingdaylights-1987" "/home/bond/post80s/timothy/licencetokill-1989")
+    FILES=("$BASEDIR/post80s/timothy/livingdaylights-1987" "$BASEDIR/post80s/timothy/licencetokill-1989")
     fi
     if [[ "$GROUPID" == "post80s" && "$USERID" == "pierce" ]]; then
-    FILES=("/home/bond/post80s/pierce/goldeneye-1995" "/home/bond/post80s/pierce/tomorrowneverdies-1997" "/home/bond/post80s/pierce/worldisnotenough-1999" "/home/bond/post80s/pierce/dieanotherday-2002")
+    FILES=("$BASEDIR/post80s/pierce/goldeneye-1995" "$BASEDIR/post80s/pierce/tomorrowneverdies-1997" "$BASEDIR/post80s/pierce/worldisnotenough-1999" "$BASEDIR/post80s/pierce/dieanotherday-2002")
     fi
     if [[ "$GROUPID" == "post80s" && "$USERID" == "daniel" ]]; then
-    FILES=("/home/bond/post80s/daniel/casinoroyale-2006" "/home/bond/post80s/daniel/quantumofsolace-2008" "/home/bond/post80s/daniel/skyfall-2012" "/home/bond/post80s/daniel/spectre-2015" "/home/bond/post80s/daniel/notimetodie-2021")
+    FILES=("$BASEDIR/post80s/daniel/casinoroyale-2006" "$BASEDIR/post80s/daniel/quantumofsolace-2008" "$BASEDIR/post80s/daniel/skyfall-2012" "$BASEDIR/post80s/daniel/spectre-2015" "$BASEDIR/post80s/daniel/notimetodie-2021")
     fi
     echo $GROUPID $USERID | tee -a $outfile
     echo " " | tee -a $outfile
@@ -148,56 +150,66 @@ done
 done
 done
 
-if [[ "`stat -c %A /home/bond/pre80s/roger`" == "lrwxrwxrwx" ]]; then
-    echo -e "symlink: /home/bond/pre80s/roger - exists" | tee -a $outfile
+if [[ "`stat -c %A $BASEDIR/pre80s/roger`" == "lrwxrwxrwx" ]]; then
+    echo -e "symlink: $BASEDIR/pre80s/roger - exists" | tee -a $outfile
 else
-    echo -e "symlink: /home/bond/pre80s/roger - DOES NOT exist - try again" | tee -a $outfile
+    echo -e "symlink: $BASEDIR/pre80s/roger - DOES NOT exist - try again" | tee -a $outfile
 fi 
-if [[ "`stat -c %A /home/bond/post80s/roger`" == "lrwxrwxrwx" ]]; then
-    echo -e "symlink: /home/bond/post80s/roger - exists" | tee -a $outfile
+if [[ "`stat -c %A $BASEDIR/post80s/roger`" == "lrwxrwxrwx" ]]; then
+    echo -e "symlink: $BASEDIR/post80s/roger - exists" | tee -a $outfile
 else
-    echo -e "symlink: /home/bond/post80s/roger - DOES NOT exist - try again" | tee -a $outfile
+    echo -e "symlink: $BASEDIR/post80s/roger - DOES NOT exist - try again" | tee -a $outfile
 fi 
 echo " " | tee -a $outfile
-if [ -e /home/bond/scaramanga.txt ]; then
-    echo -e "file: /home/bond/scaramanga.txt - exists" | tee -a $outfile
+if [ -e $BASEDIR/scaramanga.txt ]; then
+    echo -e "file: $BASEDIR/scaramanga.txt - exists" | tee -a $outfile
 else
-    echo -e "file: /home/bond/scaramanga.txt - DOES NOT exist - try again" | tee -a $outfile
+    echo -e "file: $BASEDIR/scaramanga.txt - DOES NOT exist - try again" | tee -a $outfile
 fi
-if [[ "`cat /home/bond/scaramanga.txt | wc -l`" == "31" ]]; then
-    echo -e "file: /home/bond/scaramanga.txt - correct number of lines" | tee -a $outfile
-    cat /home/bond/scaramanga.txt | tail -5 | tee -a $outfile
+if [[ "`cat $BASEDIR/scaramanga.txt | wc -l`" == "31" ]]; then
+    echo -e "file: $BASEDIR/scaramanga.txt - correct number of lines" | tee -a $outfile
+    cat $BASEDIR/scaramanga.txt | tail -5 | tee -a $outfile
 else
-    echo -e "file: /home/bond/scaramanga.txt - incorrect number of lines `wc -l < /home/bond/scaramanga.txt` - try again" | tee -a $outfile
-fi
-echo " " | tee -a $outfile
-
-if [ -e /home/bond/70sflicks.txt ]; then
-    echo -e "file: /home/bond/70sflicks.txt - exists" | tee -a $outfile
-else
-    echo -e "file: /home/bond/70sflicks.txt - DOES NOT exist - try again" | tee -a $outfile
-fi
-if [[ "`cat /home/bond/70sflicks.txt | wc -l`" == "5" ]]; then
-    echo -e "file: /home/bond/70sflicks.txt - correct number of lines" | tee -a $outfile
-    cat /home/bond/70sflicks.txt >> $outfile
-else
-    echo -e "file: /home/bond/70sflicks.txt - incorrect number of lines `wc -l < /home/bond/70sflicks.txt` - try again" | tee -a $outfile
+    echo -e "file: $BASEDIR/scaramanga.txt - incorrect number of lines `wc -l < $BASEDIR/scaramanga.txt` - try again" | tee -a $outfile
 fi
 echo " " | tee -a $outfile
 
-
-if [ -e /home/bond/usedspace.txt ]; then
-    echo -e "file: /home/bond/usedspace.txt - exists" | tee -a $outfile
-    cat /home/bond/usedspace.txt >> $outfile
+if [ -e $BASEDIR/70sflicks.txt ]; then
+    echo -e "file: $BASEDIR/70sflicks.txt - exists" | tee -a $outfile
 else
-    echo -e "file: /home/bond/usedspace.txt - DOES NOT exist - try again" | tee -a $outfile
+    echo -e "file: $BASEDIR/70sflicks.txt - DOES NOT exist - try again" | tee -a $outfile
+fi
+if [[ "`cat $BASEDIR/70sflicks.txt | wc -l`" == "5" ]]; then
+    echo -e "file: $BASEDIR/70sflicks.txt - correct number of lines" | tee -a $outfile
+    cat $BASEDIR/70sflicks.txt >> $outfile
+else
+    echo -e "file: $BASEDIR/70sflicks.txt - incorrect number of lines `wc -l < $BASEDIR/70sflicks.txt` - try again" | tee -a $outfile
+fi
+echo " " | tee -a $outfile
+
+
+if [ -e $BASEDIR/usedspace.txt ]; then
+    echo -e "file: $BASEDIR/usedspace.txt - exists" | tee -a $outfile
+    cat $BASEDIR/usedspace.txt >> $outfile
+else
+    echo -e "file: $BASEDIR/usedspace.txt - DOES NOT exist - try again" | tee -a $outfile
 fi
 
-if [ -e /home/linuxuser/bondassignment.tar.gz ]; then
-    echo -e "file: /home/linuxuser/bondassignment.tar.gz - exists" | tee -a $outfile
-    tar -tzvf /home/linuxuser/bondassignment.tar.gz | head -5 | tee -a $outfile
+if [ -e /home/linuxuser/bondlab.tar.gz ]; then
+    echo -e "file: /home/linuxuser/bondlab.tar.gz - exists" | tee -a $outfile
+    file /home/linuxuser/bondlab.tar.gz | tee -a $outfile
+    tar -tzvf /home/linuxuser/bondlab.tar.gz | head -5 | tee -a $outfile
 else
-    echo -e "file: /home/linuxuser/bondassignment.tar.gz - DOES NOT exist - try again" | tee -a $outfile
+    echo -e "file: /home/linuxuser/bondlab.tar.gz - DOES NOT exist - try again" | tee -a $outfile
+fi
+echo " " | tee -a $outfile
+
+if [ -e /home/linuxuser/bondlab.tar.bz2 ]; then
+    echo -e "file: /home/linuxuser/bondlab.tar.bz2 - exists" | tee -a $outfile
+    file /home/linuxuser/bondlab.tar.bz2 | tee -a $outfile
+    tar -tjvf /home/linuxuser/bondlab.tar.bz2 | head -5 | tee -a $outfile
+else
+    echo -e "file: /home/linuxuser/bondlab.tar.bz2 - DOES NOT exist - try again" | tee -a $outfile
 fi
 
 # Submit the work
