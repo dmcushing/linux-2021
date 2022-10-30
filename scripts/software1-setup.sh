977a6ac75d2f5fed49d936c7c10c1c7ef6d733e6
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-10-30 10:34:34 -0400

    Update software1-setup.sh

--- scripts/software1-setup.sh
+++ scripts/software1-setup.sh
@@ -8,5 +8,6 @@ is_super_user
 
 echo -e "Software I set up - ignore any errors below"
 echo -e "..Working"
+apt -y purge ninvaders moon-buggy
 apt -y install libncurses5 libtinfo5
 echo -e "....All set"
