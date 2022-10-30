daa1a614665b3bcec97e03ac7732fd4249503e67
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-10-30 10:33:01 -0400

    Create software1-setup.sh

---
+++ scripts/software1-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Fishing for grep Setup Script
+
+source /scripts/functions.sh
+clear
+is_super_user
+
+echo -e "Software I set up - ignore any errors below"
+echo -e "..Working"
+apt -y install libncurses5 libtinfo5
+echo -e "....All set"
