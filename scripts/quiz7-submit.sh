3a90cad5a6b7731b077a73bdad804389d7af8b46
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-11-17 08:12:19 -0500

    Update quiz7-submit.sh

--- scripts/quiz7-submit.sh
+++ scripts/quiz7-submit.sh
@@ -9,14 +9,16 @@ clear
 is_super_user
 student_info File System Quiz
 
-tree ~/Quiz07 | tee -a $outfile
+tree ~/Quiz7 | tee -a $outfile
 blank_line
 
-lsblk -l -o NAME,SIZE,RO,FSTYPE,MOUNTPOINT /dev/sde | tee -a $outfile
+lsblk -l -o NAME,SIZE,RO,FSTYPE,MOUNTPOINT /dev/sde /dev/sdf | tee -a $outfile
+blank_line
+mount | grep "sde\|sdf" | tee -a $outfile
 blank_line
 
 entity_exists 4 quiz7 passwd
-user_param 4 account_expiry quiz7 2023-11-18
+user_param 4 account_expiry quiz7 2037-11-08
 user_param 4 user_in_group quiz7 linuxuser
 check_existence 4 "/home/quiz7" d
 blank_line
@@ -30,4 +32,4 @@ check_existence 6 ~/Quiz07/YaTa.tar.gz f
 tar tzvf ~/Quiz07/YaTa.tar.gz | tail -5 | tee -a $outfile
 blank_line
 
-mail_out File System Quiz
+mail_out Filesystem Quiz7
