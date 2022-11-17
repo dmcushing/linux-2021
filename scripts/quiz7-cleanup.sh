0210c417ea97f7bd16417624ce06564135074766
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-11-17 08:00:49 -0500

    quiz7

--- scripts/quiz7-cleanup.sh
+++ scripts/quiz7-cleanup.sh
@@ -4,8 +4,6 @@ source /scripts/functions.sh
 clear
 is_super_user
 echo -e "Cleaning up..."
-umount /dev/sde1 2>/dev/null
-sgdisk --zap-all /dev/sde 2>/dev/null
 userdel -r quiz7 2>/dev/null
 rm -Rf /home/linuxuser/Quiz07
 echo -e "Done clean up."
--- scripts/quiz7-setup.sh
+++ scripts/quiz7-setup.sh
@@ -4,10 +4,8 @@ source /scripts/functions.sh
 clear
 is_super_user
 echo -e "Setting up..."
-umount /dev/sde1 2>/dev/null
-sgdisk --zap-all /dev/sde 2>/dev/null
 userdel -r quiz7 2>/dev/null
-rm -Rf /home/linuxuser/Quiz07
-mkdir /home/linuxuser/Quiz07
-chown linuxuser:linuxuser /home/linuxuser/Quiz07
+rm -Rf /home/linuxuser/Quiz7
+mkdir /home/linuxuser/Quiz7
+chown linuxuser:linuxuser /home/linuxuser/Quiz7
 echo -e "Done setting up."
