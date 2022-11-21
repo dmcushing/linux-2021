80435825af0c599083a509eb595b6191152af1f7
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-11-21 08:16:40 -0500

    Update core-submit.sh

--- scripts/core-submit.sh
+++ scripts/core-submit.sh
@@ -5,7 +5,7 @@ clear
 is_super_user
 student_info Core Services
 
-check_existence 1 mailpeople group
+entity_exists 1 mailpeople group
 blank_line
 entity_exists 2 mailperson1 passwd
 check_existence 2 /home/mailperson1 d
@@ -22,8 +22,8 @@ blank_line
 echo -e "Was mail sent to mailperson1?" | tee -a $outfile
 check_existence 4 /var/mail/mailperson1 f
 tail -7 /var/mail/mailperson1 2>/dev/null | tee -a $outfile
-echo -e "4. grep /var/log/syslog for mailperson" | tee -a $outfile
 blank_line
+echo -e "grep /var/log/syslog for mailperson1" | tee -a $outfile
 check_existence 5 ~/corelab/mailperson.log f
 tail -5 ~/corelab/mailperson.log 2>/dev/null | tee -a $outfile
 blank_line
