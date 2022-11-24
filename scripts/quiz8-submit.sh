393b0bfb91862a11b0992bbaed7e3c3a14739355
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-11-24 10:20:28 -0500

    Update quiz8-submit.sh

--- scripts/quiz8-submit.sh
+++ scripts/quiz8-submit.sh
@@ -3,11 +3,11 @@ source /scripts/functions.sh
 clear
 is_super_user
 student_info Quiz 8
-lsblk -l -o NAME,SIZE,FSTYPE,MOUNTPOINT,RO /dev/vg_quiz8/lv_quiz8 | tee -a $outfile
+lsblk -l -o NAME,SIZE,FSTYPE,PARTTYPENAME,MOUNTPOINT,RO /dev/vg_quiz8/lv_quiz8 | tee -a $outfile
 blank_line
-mount | grep 'media' | tee -a $outfile
+mount | grep "media" | tee -a $outfile
 blank_line
-pvdisplay --short
+pvdisplay --short | tee -a $outfile
 blank_line
 vgdisplay vg_quiz8 | tee -a $outfile
 blank_line
