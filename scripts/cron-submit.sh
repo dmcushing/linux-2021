61cf9cecde4da67c9eee33d5cfa27248129b890d
Dave Cushing <dave.cushing@cambriancollege.ca>
2022-11-28 16:09:24 -0500

    Update cron-submit.sh

--- scripts/cron-submit.sh
+++ scripts/cron-submit.sh
@@ -10,7 +10,7 @@ grep "^[^#;]" /var/spool/cron/crontabs/root | tee -a $outfile
 blank_line
 grep "^[^#;]" /var/spool/cron/crontabs/linuxuser | tee -a $outfile
 blank_line
-check_existence - ~/cronlab/cronlab.tar.gz
+check_existence - ~/cronlab/cronlab.tar.gz f
 file ~/cronlab/cronlab.tar.gz | tee -a $outfile
-tar tzvf ~/cronlab/cronlab.tar.gz | tee -a $outfile
+tar tzf ~/cronlab/cronlab.tar.gz | tee -a $outfile
 mail_out cron Lab
