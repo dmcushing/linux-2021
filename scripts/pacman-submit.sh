608af81908a6e37a8f8613ede5e89934d8911c49
Dave Cushing <dave@davecushing.ca>
2023-07-16 07:22:57 -0400

    update

--- scripts/pacman-submit.sh
+++ scripts/pacman-submit.sh
@@ -7,13 +7,15 @@ student_info PacMan Lab
 blank_line
 echo -e "All ghosts should be in /home/linuxuser/quarantine" | tee -a $outfile
 blank_line
-check_existence 1 /home/linuxuser/quarantine/8linky.txt
-check_existence 1 /home/linuxuser/quarantine/PiNkY.txt
-check_existence 1 /home/linuxuser/quarantine/sue.txt
-check_existence 1 /home/linuxuser/quarantine/CLYDE.txt
-check_existence 1 /home/linuxuser/quarantine/funky.txt
-check_existence 1 /home/linuxuser/quarantine/1nky.txt
-check_existence 1 /home/linuxuser/quarantine/sp00ky.txt
+check_existence 1 /home/linuxuser/quarantine/ d
+blank_line
+check_existence 1 /home/linuxuser/quarantine/8linky.txt f
+check_existence 1 /home/linuxuser/quarantine/PiNkY.txt f
+check_existence 1 /home/linuxuser/quarantine/sue.txt f
+check_existence 1 /home/linuxuser/quarantine/CLYDE.txt f
+check_existence 1 /home/linuxuser/quarantine/funky.txt f
+check_existence 1 /home/linuxuser/quarantine/1nky.txt f
+check_existence 1 /home/linuxuser/quarantine/sp00ky.txt f
 blank_line
 
 mail_out PacMan Lab CET1025
