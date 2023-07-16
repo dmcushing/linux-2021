589f665690f47642beb3b9e9786a9b3f89f4feff
Dave Cushing <dave@davecushing.ca>
2023-07-16 07:20:19 -0400

    Update pacman-submit.sh

--- scripts/pacman-submit.sh
+++ scripts/pacman-submit.sh
@@ -4,9 +4,16 @@ clear
 is_super_user
 student_info PacMan Lab
 
-echo -e "All files should be in /home/linuxuser/quarantine - none in /home/linuxuser/Pacman" | tee -a $outfile
 blank_line
-find ~/ \( -name "sp00ky.txt" -o -name "CLYDE.txt" -o -name "8linky.txt" -o -name "PiNkY.txt" -o -name "1nky.txt" -o -name "sue.txt" -o -name "funky.txt" \) | tee -a $outfile
+echo -e "All ghosts should be in /home/linuxuser/quarantine" | tee -a $outfile
+blank_line
+check_existence 1 /home/linuxuser/quarantine/8linky.txt
+check_existence 1 /home/linuxuser/quarantine/PiNkY.txt
+check_existence 1 /home/linuxuser/quarantine/sue.txt
+check_existence 1 /home/linuxuser/quarantine/CLYDE.txt
+check_existence 1 /home/linuxuser/quarantine/funky.txt
+check_existence 1 /home/linuxuser/quarantine/1nky.txt
+check_existence 1 /home/linuxuser/quarantine/sp00ky.txt
 blank_line
 
 mail_out PacMan Lab CET1025
