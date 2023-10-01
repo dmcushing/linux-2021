b7a7ff78aed743f620e489e028b2d537be805784
Dave Cushing <dave@davecushing.ca>
2023-10-01 11:35:51 -0400

    Update archiving-setup.sh

--- scripts/archiving-setup.sh
+++ scripts/archiving-setup.sh
@@ -10,4 +10,24 @@ rm -Rf ~/archiving
 mkdir ~/archiving
 chown linuxuser:linuxuser ~/archiving
 cp /scripts/work/whitethroats ~/archiving/unsorted.txt
+# install bzip2 if needed
+package_name="bzip2"
+
+# Check if the package is installed
+if ! dpkg -l | grep -q "^ii  $package_name "; then
+    echo "Package '$package_name' is not installed. Installing..."
+    
+    # Use your package manager to install the package
+    apt-get update
+    apt-get install -y $package_name
+    
+    # Check if the installation was successful
+    if [ $? -eq 0 ]; then
+        echo "Package '$package_name' has been successfully installed."
+    else
+        echo "Error: Failed to install package '$package_name'."
+    fi
+else
+    echo "Package '$package_name' is already installed."
+fi
 echo -e "Done setting up."
