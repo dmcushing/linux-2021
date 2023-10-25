426fb36cb04372aa1eadc26d2a26f4c044f4ac01
Dave Cushing <dave@davecushing.ca>
2023-10-25 18:58:22 -0400

    Update software1-setup.sh

--- scripts/software1-setup.sh
+++ scripts/software1-setup.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# Fishing for grep Setup Script
-
 source /scripts/functions.sh
 clear
 is_super_user
 
-echo -e "Software I set up - ignore any errors below"
+echo -e "Setting up - ignore any errors below"
 echo -e "..Working"
 apt -y purge ninvaders moon-buggy
 apt -y install libncurses5 libtinfo5
